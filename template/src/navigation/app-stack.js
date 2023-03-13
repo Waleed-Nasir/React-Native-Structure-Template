5846e8591af8e866020173bb2a6a47b79aa77f9c
Waleed Nasir <33671736+Waleed-Nasir@users.noreply.github.com>
2023-03-13 19:58:46 +0500

    Created Video calling with React-naitve firestore and webrtc

--- template/src/navigation/app-stack.js
+++ template/src/navigation/app-stack.js
@@ -2,13 +2,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { screens } from '../config';
 import Home from '../screens/home';
+import VideoCallScreen from '../screens/VideoCalling';
 
 const MainStack = createStackNavigator();
 
 export default function MainNavigator() {
   return (
     <MainStack.Navigator headerMode="none">
-      <MainStack.Screen name={screens.home} component={Home} />
+      <MainStack.Screen name={screens.home} component={VideoCallScreen} />
     </MainStack.Navigator>
   );
 }
